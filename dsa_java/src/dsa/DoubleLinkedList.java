8886a797c4dfea7c3686df30d70bb42684a1ab39
oluwatobi1 <akintunlesetobi@gmail.com>
2023-03-04 23:34:15 +0100

    doubly linked list

--- dsa_java/src/dsa/DoubleLinkedList.java
+++ dsa_java/src/dsa/DoubleLinkedList.java
@@ -5,7 +5,7 @@ import linkedlist.LinkedList;
 public class DoubleLinkedList {
     private Node head;
     private Node tail;
-    private int length;
+    public int length;
 
     public class Node {
         public int value;
@@ -82,10 +82,88 @@ public class DoubleLinkedList {
             head = node;
             tail  = node;
         }else {
-            node.prev = head;
+            head.prev = node;
+            node.next = head;
             head = node;
         }
         length++;
+    }
+
+    public Node removeFirst(){
+        if (length == 0) return null;
+        Node temp = head;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        }else {
+            head = head.next;
+            head.prev = null;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
+
+    public Node get(int index){
+        if (index<0 || index >= length) return null;
+        Node temp = head;
+        if (index < length/2){
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+                System.out.println("temp" +temp.value);
+            };
+        }else {
+            temp = tail;
+            for (int i = length-1; i > index; i--) {
+                temp = temp.prev;
+                System.out.println("temp" +temp.value);
+
+            }
+        }
+        return temp;
+    }
+
+    public boolean set(int index, int value){
+        Node currNode = get(index);
+        if (currNode != null){
+            currNode.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insert(int index, int value){
+        if (index < 0 || index >= length) return false;
+        if (index == 0){
+            prepend(value);
+            return true;
+        }
+        if (index == length){
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node currNode = get(index-1 );
+        Node temp = currNode.next;
+        newNode.prev = currNode;
+        newNode.next = temp;
+        currNode.next = newNode;
+        temp.prev = currNode;
+        length++;
+        return true;
+    }
+    public Node remove(int index){
+        if (index < 0 || index >= length) return null;
+        if (index == 0)return removeFirst();
+        if (index == length-1)return removeLast();
+        Node currNode = get(index);
+        System.out.println("valss"+currNode.value);
+        currNode.next.prev = currNode.prev;
+        currNode.prev.next = currNode.next;
+        currNode.next = null;
+        currNode.prev = null;
+        length--;
+        return currNode;
     }
 
 }
--- dsa_java/src/linkedlist/Main.java
+++ dsa_java/src/linkedlist/Main.java
@@ -9,13 +9,23 @@ public class Main {
         myDoubleLinkedList.append(3);
         myDoubleLinkedList.append(4);
         myDoubleLinkedList.prepend(5);
+        myDoubleLinkedList.prepend(6);
+        myDoubleLinkedList.prepend(8);
         System.out.println("\nDouble Linked List");
         myDoubleLinkedList.printList();
         System.out.println("Double " + myDoubleLinkedList.removeLast().value);
+        myDoubleLinkedList.removeFirst();
+        System.out.println("Get " + myDoubleLinkedList.get(3).value);
+        System.out.println("Set " + myDoubleLinkedList.set(3, 5));
+        System.out.println("Set " + myDoubleLinkedList.insert(3, 10));
+
+        System.out.println("Length" + myDoubleLinkedList.length);
+        System.out.println("remove" + myDoubleLinkedList.remove(4));
 
         System.out.println("\nDouble Linked List");
         myDoubleLinkedList.printList();
 
     }
+
 
 }
